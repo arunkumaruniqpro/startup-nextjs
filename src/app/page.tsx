f7bc95653f5c51f548a0ab8edcd1d123d2f5e952
Arunkumar Subbiah <aks2023.capricon@gmail.com>
2024-02-24 07:32:51 +0000

    updated donjon about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "A SaaS based Zero Trust Network Access",
+  description: "Zero trust, first coined by academia in the 1990s, is increasingly viewed as a mainstay of modern security strategies, taking over from traditional '“castle-and-moat"' network security models in which no one outside the network can access data on the inside, but everyone inside the network can. Commercial enterprises across the globe are fast moving toward zero trust, but public sector agencies are also being pushed to adopt zero trust cybersecurity principles and adjust their network architectures in a bid to  improve national cybersecurity.",
   // other metadata
 };
 
